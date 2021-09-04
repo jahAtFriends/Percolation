11c5f881ba454e1b5b65c78f0ee06c7eb4498cf4
Joel Hammer <jhammer@friendsbalt.org>
2021-09-04 11:27:44 -0400

    created Percolation class with starter methods for student completion

---
+++ Percolation.java
@@ -0,0 +1,51 @@
+
+public class Percolation implements Percolator {
+	
+	public Percolator(int n) {
+		
+		//TODO: Constructor for an n-by-n grid.
+		
+	}
+	
+	@Override
+	public void open(int row, int col) {
+		
+		//TODO: Implement this method.
+		
+	}
+	
+	@Override 
+	public boolean isOpen(int row, int col) {
+		
+		//TODO: Implement this method.
+		
+	}
+	
+	@Override
+	public boolean hasWater(int row, int col) {
+		
+		//TODO: Implement this method.
+		
+	}
+	
+	@Override
+	public boolean percolates() {
+		
+		//TODO: Implement this method.
+		
+	}
+	
+	@Override
+	public int numberOfOpenSites() {
+		
+		//TODO: Implement this method
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		//TODO: Test your implementation!
+		
+	}
+	
+}
