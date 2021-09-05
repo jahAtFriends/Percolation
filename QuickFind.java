e6404271ca2f90113f7c283f19c3e7954e1e5f3a
Joel Hammer <jhammer@friendsbalt.org>
2021-09-04 22:02:17 -0400

    added starter classes for students

---
+++ QuickFind.java
@@ -0,0 +1,31 @@
+/*
+* Friends School adaptation of a lab by Robert Sedgewick described at
+* https://introcs.cs.princeton.edu/java/24percolation/
+*
+*
+* (c) 2021 Joel Hammer, Friends School of Baltimore.
+*/
+
+
+public class QuickFind implements UnionFinder {
+    
+    public UnionFinder(int n) {
+        
+        //TODO: Construct a dynamic connection system of n items.
+        
+    }
+    
+    @Override
+    public void union(int a, int b) {
+        
+        //TODO: Implement this method.
+        
+    }
+    
+    @Override
+    public boolean find(int a, int b) {
+        
+        //TODO: Implement this method.
+        
+    }
+}
